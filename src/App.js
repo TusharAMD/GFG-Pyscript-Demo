110779e054254355ebe3490c9dc7167b689bc0b0
Tushar <tusharvamanamdoskar@gmail.com>
2022-05-22 09:56:35 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
+
 function App() {
-
-  const [refresh,setRefresh] = useState(true)
-
-  function refreshfun(){
-    setRefresh(!refresh)
-    console.log(refresh)
-  }
+	
 
 
   return (
@@ -18,7 +12,9 @@ function App() {
 	  {/*Dependencies*/}
 	  <py-env>
 	  - numpy {"\n"}
-	  - scikit-learn
+	  - pandas {"\n"}
+	  - scikit-learn {"\n"}
+	  - matplotlib
 	  </py-env>
 	  {/*------------*/}
 	
@@ -40,8 +36,27 @@ function App() {
 	  
 
       <py-script>
+	  import pandas as pd {"\n"}
+	  import pyodide {"\n"}
+	  import matplotlib.pyplot as plt {"\n"}
+	  from sklearn import datasets, linear_model {"\n"}
+	  from sklearn.model_selection import train_test_split {"\n"}
+	  url="http://raw.githubusercontent.com/TusharAMD/GFG-Pyscript-Demo/master/data.csv" {"\n"}
+	  dataset=pd.read_csv(pyodide.http.open_url(url)) {"\n"}
 	  
-      
+	  X = dataset.iloc[:, :-1].values {"\n"}
+	  y = dataset.iloc[:, 1].values {"\n"}
+	  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) {"\n"}
+	  
+	  regressor = linear_model.LinearRegression() {"\n"}
+	  regressor.fit(X_train, y_train) {"\n"}
+	  print(regressor.coef_) {"\n"}
+	  print("X TEST") {"\n"}
+	  print(X_test) {"\n"}
+	  y_pred = regressor.predict(X_test) {"\n"}
+	  print("Predicted") {"\n"}
+	  print(y_pred)
+	  
       </py-script>
 
       
